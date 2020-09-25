b7321f080a1e7513930dd7156c3e401f1c4dc433
emipa606 <emil.offermo@litet.net>
2020-09-25 20:34:07 +0200

    First publish

---
+++ Source/SteamWorldCE/SteamWorldCE.cs
@@ -0,0 +1,34 @@
+﻿using CombatExtended;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace SteamWorldCE
+{
+
+    [StaticConstructorOnStartup]
+    static class SteamWorldCE
+    {
+        static SteamWorldCE()
+        {
+            var harmony = new Harmony("Mlie.SteamWorldCE");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        [HarmonyPatch(typeof(LetterTracker))]
+        [HarmonyPatch("MapComponentTick")]
+        public class Prefix_LetterTracker_MapComponentTick
+        {
+            [HarmonyPrefix]
+            public static void Prefix(ref bool ____sentMechWarning)
+            {
+                ____sentMechWarning = true;
+            }
+        }
+    }
+}
