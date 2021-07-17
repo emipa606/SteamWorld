296ceec1f1ecac106c438fddd445858aa7061972
emipa606 <emil.offermo@litet.net>
2021-07-17 20:21:20 +0200

    Mod updated for 1.3 and passed autotests

--- Source/SteamWorldCE/SteamWorldCE.cs
+++ Source/SteamWorldCE/SteamWorldCE.cs
@@ -1,18 +1,12 @@
-﻿using CombatExtended;
+﻿using System.Reflection;
+using CombatExtended;
 using HarmonyLib;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using Verse;
 
 namespace SteamWorldCE
 {
-
     [StaticConstructorOnStartup]
-    static class SteamWorldCE
+    internal static class SteamWorldCE
     {
         static SteamWorldCE()
         {
