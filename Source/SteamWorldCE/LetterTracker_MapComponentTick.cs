0cad0aba20407d2a74043940fca05aa031263ca9
Mlie <emipa606@users.noreply.github.com>
2025-07-05 00:22:56 +0200

    Mod updated for 1.6 and passed autotests

--- Source/SteamWorldCE/LetterTracker_MapComponentTick.cs
+++ Source/SteamWorldCE/LetterTracker_MapComponentTick.cs
@@ -4,9 +4,8 @@ using HarmonyLib;
 namespace SteamWorldCE;
 
 [HarmonyPatch(typeof(LetterTracker), nameof(LetterTracker.MapComponentTick))]
-public class Prefix_LetterTracker_MapComponentTick
+public class LetterTracker_MapComponentTick
 {
-    [HarmonyPrefix]
     public static void Prefix(ref bool ____sentMechWarning)
     {
         ____sentMechWarning = true;
